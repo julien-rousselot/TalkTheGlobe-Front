f846da8758eb6b49888d7b9329a7c92e8188dec5
Julien Rousselot <jujuf50@live.fr>
2025-03-01 20:20:19 +0100

    email fonctionnel +design

---
+++ src/components/Email/Email.tsx
@@ -0,0 +1,60 @@
+// import { useState } from 'react';
+import { useState } from 'react';
+import EmailLogo from '../../assets/Email.png';
+
+const Email = () => {
+const [sent, setSent] = useState('');
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+    const form = e.target as HTMLFormElement;
+    const formdata = new FormData(form);
+
+    const email =  formdata.get('email');
+    const message = formdata.get('message');
+    const name = formdata.get('name');
+
+     await fetch("http://localhost:8000/api/send-email", {
+          method: "POST",
+          headers: {
+              "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ email, message, name }),
+      });
+      setSent('Message Envoyé');
+      console.log("name", name);
+      form.reset();
+  };
+
+    return (
+      <section className='p-8 '>
+        <h3 className='text-center text-redTitle text-4xl flex flex-col'>CHOOSE THE BEST OPTION FOR YOU!</h3>     
+        <div className='flex flex-col md:flex-row text-center justify-around'>
+            <div className='m-auto'>
+              <img src={EmailLogo} alt="" className='p-6 md:p-0' /> 
+            </div>
+            <form onSubmit={handleSubmit} className='flex flex-col my-6 md:w-1/2'>
+              <div className='flex flex-col md:flex-row'>
+                <div className='flex flex-col md:pr-12 md:w-1/2'> 
+                  <label htmlFor="name" className='text-left font-semibold text-[#262a38]'>Name</label>
+                  <input type="text" name='name' className="border-2 border-gray-500 p-1 my-2 mb-4 rounded-md bg-gray-200 text-left focus:border-[#FFE7A3] focus:outline-none" />
+                </div>
+                <div className='flex flex-col md:w-1/2'>                  
+                  <label htmlFor="email" className='text-left font-semibold text-[#262a38]'>Email</label>
+                  <input type="email" name='email' required className='border-2 border-gray-500 p-1 my-2 mb-4 rounded-md bg-gray-200 text-left focus:border-[#FFE7A3] focus:outline-none'/>
+                </div>
+              </div>
+              <div className='flex flex-col'>
+                <label htmlFor="message" className='text-left font-semibold text-[#262a38]'>Message</label>
+                <textarea name='message' className='border-2 border-gray-500 p-1 my-2 mb-4 h-32 rounded-md bg-gray-200 text-left focus:border-[#FFE7A3] focus:outline-none'/>
+              </div>
+              
+              <button className='bg-[#FFE7A3] text-center border-2 border-[#667175] p-2 rounded-md font-semibold' type="submit">Send</button>
+              <p className='font-semibold pt-6'>{sent}</p>
+            </form>    
+        </div>
+      </section>
+    );
+  };
+  
+  export default Email;
+  
--- src/components/Home/Services.tsx
+++ src/components/Home/Services.tsx
@@ -3,6 +3,7 @@ import OneToOne from '../../assets/onetoone.png';
 import SemiPrivate from '../../assets/semi-private.png';
 import Group from '../../assets/Group.png';
 import Banner from '../Banner/Banner';
+import Email from '../Email/Email';
 
 
 const Services = () => {
@@ -10,47 +11,48 @@ const Services = () => {
     <section>
         <Banner Title="LEARN ENGLISH AND SPANISH WITH ME!" Subtitle="Personalized lessons tailored to your  goals, whether you're learning for work, travel, or personal growth" Image={Blackboard} ButtonText=""/>
         <main className="bg-[url('assets/Backgroundblanc.jpg')] w-full">
-            <h3 className='text-center text-redTitle'>CHOOSE THE BEST OPTION FOR YOU!</h3>
-            <div className='flex flex-row'>
-                <div className='bg-purple flex-1'>
-                    <h4 className='text-3xl font-semibold'>📍 1-on-1 Private Lessons</h4>
+            <h3 className='text-center text-redTitle text-4xl p-10'>CHOOSE THE BEST OPTION FOR YOU!</h3>
+            <div className='flex flex-col-reverse md:flex-row'>
+                <div className='bg-purple flex-1 pb-8 md:pb-0'>
+                    <h4 className='text-3xl p-8'>📍 1-on-1 Private Lessons</h4>
                     <ul>
-                        <li className='text-3xl font-semibold'>Personalized lessons tailored to your level and goals.</li>
-                        <li className='text-3xl font-semibold'>Flexible scheduling to fit your lifestyle.</li>
-                        <li className='text-3xl font-semibold'>Perfect for rapid progress and customized learning.</li>
+                        <li className='text-3xl px-8 pb-6 md:px-12'>Personalized lessons tailored to your level and goals.</li>
+                        <li className='text-3xl px-8 pb-6 md:px-12'>Flexible scheduling to fit your lifestyle.</li>
+                        <li className='text-3xl px-8 pb-6 md:px-12'>Perfect for rapid progress and customized learning.</li>
                     </ul>                   
                 </div>
-                <div className='flex-1'>
+                <div className='flex-1 flex justify-center'>
                     <img src={OneToOne} alt="" />
                 </div>
             </div>
-            <div className='flex flex-row'>
-                <div className='flex-1'>
+            <div className='flex flex-col md:flex-row'>
+                <div className='flex-1 flex justify-center'>
                     <img src={SemiPrivate} alt="picture representing a group of students for a course"/>
                 </div>
-                <div className='bg-green flex-1'>
-                    <h4 className='text-3xl font-semibold'>👥 Semi-Private Lessons (2 People)</h4>
+                <div className='bg-green flex-1 justify-center' >
+                    <h4 className='text-3xl p-8'>👥 Semi-Private Lessons (2 People)</h4>
                     <ul>
-                        <li className='text-3xl font-semibold'>Ideal for friends or family learning together.</li>
-                        <li className='text-3xl font-semibold'>Same level required for both students.</li>
-                        <li className='text-3xl font-semibold'>Interactive and engaging lessons at a lower price per person.</li>
+                        <li className='text-3xl px-8 pb-6 md:px-12'>Ideal for friends or family learning together.</li>
+                        <li className='text-3xl px-8 pb-6 md:px-12'>Same level required for both students.</li>
+                        <li className='text-3xl px-8 pb-6 md:px-12'>Interactive and engaging lessons at a lower price per person.</li>
                     </ul>
                 </div>
             </div>
-            <div className='flex flex-row'>
-                <div className='bg-yellow flex-1'>
-                    <h4 className='text-3xl font-semibold'>🌍 Small Group Classes (3-6 People) — Coming Soon!</h4>
+            <div className='flex flex-col-reverse md:flex-row'>
+                <div className='bg-[#ffe8a3] flex-1'>
+                    <h4 className='text-3xl p-8'>🌍 Small Group Classes (3-6 People) — Coming Soon!</h4>
                     <ul>
-                        <li className='text-3xl font-semibold'>Join a small, supportive learning community.</li>
-                        <li className='text-3xl font-semibold'>Fun, interactive lessons covering all skills (speaking, listening, reading, and writing).</li>
-                        <li className='text-3xl font-semibold'>Great for motivation and conversation practice.</li>
+                        <li className='text-3xl px-8 pb-6 md:px-12'>Join a small, supportive learning community.</li>
+                        <li className='text-3xl px-8 pb-6 md:px-12'>Fun, interactive lessons covering all skills (speaking, listening, reading, and writing).</li>
+                        <li className='text-3xl px-8 pb-6 md:px-12'>Great for motivation and conversation practice.</li>
                     </ul>
                 </div>
-                <div className='flex-1'>
+                <div className='flex-1 flex justify-center'>
                     <img src={Group} alt="picture representing a group of students for a course"/>
                 </div>
             </div>
         </main>
+        <Email/>
     </section>
   );
 };
